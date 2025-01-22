9a818e593dbd8f239286f38cf733d2369c011692
Carlos Antonio <carlosabcj1@gmail.com>
2025-01-22 17:17:23 -0300

    operadores

---
+++ fundamentos/operadores.js
@@ -0,0 +1,39 @@
+/**
+ * OPERADORES
+ * - Aritméticos
+ * - Aritméticos de atribuição
+ * - Comparação
+ * - Lógicos
+ */
+
+console.clear()
+console.log("Operadores atirméticos")
+console.log("+ | soma")
+console.log("- | subtração")
+console.log("* | multiplicação")
+console.log("/ | divisão")
+console.log("% | módulo (resto da divisão)")
+console.log("")
+
+console.log("Operadores atirméticos de atribuição")
+console.log("+ | soma e atribuição a variável")
+console.log("- | subtração e atribuição a variável")
+console.log("* | multiplicação e atribuição a variável")
+console.log("/ | divisão e atribuição a variável")
+console.log("++ | somar 1 ao resultado")
+console.log("-- | subtrair 1 ao resultado")
+console.log("")
+
+console.log("comparadores")
+console.log("> | maior que")
+console.log("< | menor que")
+console.log(">= | maior ou igual a")
+console.log("<= | menor ou igual a")
+console.log("=== | idêntico a")
+console.log("!== | não idêntico a")
+console.log("")
+
+console.log("Operadores lógicos")
+console.log("&& | AND")
+console.log("|| | OR")
+console.log("! | NOT")
